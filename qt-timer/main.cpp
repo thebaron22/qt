6450cc8f757d8cb8389b68eb6e4288f56397e987
Mick <the8aron@yahoo.com>
2020-03-10 21:47:08 +1100

    qt-timer

---
+++ qt-timer/main.cpp
@@ -0,0 +1,29 @@
+#include <QtGui>
+#include <QApplication>
+#include <QLabel>
+
+#include "mytimer.h"
+
+int main(int argc, char **argv) {
+
+ QApplication app(argc, argv);
+
+ QObject *parent;
+ QString program = "astrolog";
+ QStringList arguments;
+ QProcess *myProcess = new QProcess(parent);
+ myProcess->start(program, arguments);
+
+ QPixmap chart("/tmp/astro.log");
+
+ QLabel label;
+ label.setWindowFlags(Qt::Window | Qt::FramelessWindowHint);
+ label.setStyleSheet("background-color: dimgray");
+ label.setPixmap(chart);
+ label.showMaximized();
+
+ MyTimer fred;
+
+ return app.exec();
+
+}
